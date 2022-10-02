ad83a779be44098c225a178c974e02fa155b3b25
Yasin Altunisik <altun.yasin.isik@gmail.com>
2022-10-03 00:40:46 +0300

    first divs and spans

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>NotesApp</h1>
+      </div>
+
+      <div>
+        <form>
+          <label>
+            <input type="text" placeholder="Search..." />
+          </label>
+        </form>
+      </div>
+
+      <div className="text">
+        <div>
+          <form>
+            <label>
+              <input type="text" placeholder="Search..." />
+            </label>
+          </form>
+        </div>
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 }
