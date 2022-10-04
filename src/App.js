735b9c4e3acf4e23bf5e7f61eda2366f35ef91f5
Yasin Altunışık <altun.yasin.isik@gmail.com>
2022-10-04 13:31:12 +0300

    developing note-app UI

--- src/App.js
+++ src/App.js
@@ -3,31 +3,34 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>NotesApp</h1>
+      <div className="ex1">
+        <text>NotesApp</text>
       </div>
 
       <div>
-        <form>
+        <form id="form1">
           <label>
-            <input type="text" placeholder="Search..." />
+            <input id="input1" type="text" placeholder="   Search..." />
           </label>
         </form>
       </div>
 
-      <div className="text">
+      <div className="ex2">
+
         <div>
           <form>
             <label>
-              <input type="text" placeholder="Search..." />
+              <input type="text" placeholder="   Enter your note here..." />
             </label>
           </form>
         </div>
+
         <div>
           <span></span>
           <span></span>
         </div>
       </div>
+
     </div>
   );
 }
